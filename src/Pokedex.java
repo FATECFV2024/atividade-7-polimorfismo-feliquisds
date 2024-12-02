29e7f05fda0b829aee865952daa240c064cb8400
Felix <mail@feliquisds.gay>
2024-12-02 11:53:06 -0300

    feat(Pokedex); update Pokedex with new content

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -1,23 +1,25 @@
 public class Pokedex {
     public static void main(String[] args) throws Exception {
-        Eevee e = new Eevee();
-        Jolteon j = new Jolteon();
+        Eevee ee = new Eevee(30, 55, 6.5);
+        Vaporeon va = new Vaporeon(100, 130, 29);
+        Jolteon jo = new Jolteon(80, 65, 24.5);
+        Flareon fl = new Flareon(90, 65, 25);
+        Espeon es = new Espeon(90, 65, 26.5);
+        Umbreon um = new Umbreon(100, 95, 27);
+        Leafeon le = new Leafeon(100, 65, 25.5);
+        Glaceon gl = new Glaceon(80, 65, 25.9);
+        Sylveon sy = new Sylveon(100, 95, 23.5);
 
-        System.out.println("Eu sou: Eevee");
-        System.out.println("Ataque: " + e.ataque());
-        System.out.println("Defesa: " + e.defesa());
-        System.out.println("Especial: " + e.especial());
 
-        System.out.println("");
 
-        System.out.println("Eu sou: Jolteon");
-        System.out.println("Ataque: " + j.ataque());
-        System.out.println("Defesa: " + j.defesa());
-        System.out.println("Especial: " + j.especial());
-
-        e = j;
-        System.out.println("Ataque: " + e.ataque());
-        System.out.println("Defesa: " + e.defesa());
-        System.out.println("Especial: " + e.especial());
+        ee.printInfo();
+        va.printInfo();
+        jo.printInfo();
+        fl.printInfo();
+        es.printInfo();
+        um.printInfo();
+        le.printInfo();
+        gl.printInfo();
+        sy.printInfo();
     }    
 }
