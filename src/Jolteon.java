7efdece8f739822bbdc1ba236f8d16f44d2a671c
Felix <mail@feliquisds.gay>
2024-12-02 09:20:58 -0300

    feat(Jolteon); added Jolteon class

---
+++ src/Jolteon.java
@@ -0,0 +1,16 @@
+public class Jolteon extends Eevee {
+    @Override
+    public String ataque() {
+        return "Trovoada";
+    }
+
+    @Override
+    public String defesa() {
+        return "Carga";
+    }
+
+    @Override
+    public String especial() {
+        return "Raio";
+    }
+}
