77f8f13a280f528981dc644f3ad7469845ed8fb1
Felix <mail@feliquisds.gay>
2024-12-02 09:49:31 -0300

    feat(Eevee); added constructor, getters, setters and default type

--- src/Eevee.java
+++ src/Eevee.java
@@ -1,7 +1,57 @@
 public class Eevee {
-    private String tipo;
+    private String tipo = "Normal";
     private int altura, HP;
     private double peso;
+
+
+
+    public Eevee(int altura, int HP, double peso) {
+        this.altura = altura;
+        this.HP = HP;
+        this.peso = peso;
+    }
+
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    protected void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
 
     public String ataque() {
         return "Ataque rápido";
