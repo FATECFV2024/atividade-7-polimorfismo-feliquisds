39247d51003c61133607b0db6c9998726a1350bf
Felix <mail@feliquisds.gay>
2024-12-02 10:13:18 -0300

    feat(Jolteon); added constructors

--- src/Jolteon.java
+++ src/Jolteon.java
@@ -1,4 +1,15 @@
 public class Jolteon extends Eevee {
+    public Jolteon(String nome, int altura, int HP, double peso) {
+        super(nome, altura, HP, peso);
+        setTipo("Elétrico");
+    }
+
+    public Jolteon(int altura, int HP, double peso) {
+        super(altura, HP, peso);
+        setNome("Jolteon");
+        setTipo("Elétrico");
+    }    
+
     @Override
     public String ataque() {
         return "Trovoada";
